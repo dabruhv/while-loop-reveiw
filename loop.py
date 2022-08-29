6e90a9ed8309ba468e3401aae1c525a70e11ae62
dabruhv <78553141+dabruhv@users.noreply.github.com>
2022-08-29 08:09:27 -0600

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,12 @@
+doExit = False
+sussy = 'a'
+
+while doExit is False:
+    print("gimmie letter")
+    sussy = input()
+    if sussy == 'q':
+        doExit = True
+    else:
+        print("you pressed ",sussy)
+
+print("bye bye")
